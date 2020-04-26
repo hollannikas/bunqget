eb76941fed3731abd7d2a68b4b485e3d65ea5e90
Rudolf Poels <rudolf.poels@futurice.com>
2020-04-26 09:11:22 +0300

    list payments
    Connect to production and list all payments of all accounts

--- src/main/kotlin/BunqGet.kt
+++ src/main/kotlin/BunqGet.kt
@@ -3,26 +3,44 @@ import com.bunq.sdk.context.ApiEnvironmentType
 import com.bunq.sdk.context.BunqContext
 import com.bunq.sdk.http.Pagination
 import com.bunq.sdk.model.generated.endpoint.MonetaryAccountBank
+import com.bunq.sdk.model.generated.endpoint.Payment
 import java.io.File
 
-fun main(args: Array<String>) {
+fun main() {
     loadAPIContext()
 
-    val pagination = Pagination()
-    pagination.count = 1
+    val activeAccounts = getActiveAccounts()
+
+    val paymentsPerAccount = activeAccounts
+        .map { it to getPayments(it.id) }
+        .toMap()
     
-    MonetaryAccountBank
-        .list(pagination.urlParamsCountOnly).value
-        .filter { it.status == "ACTIVE" }
-        .map { "Account ${it.id} has a balance of ${it.balance.value} ${it.balance.currency}" }
+    paymentsPerAccount.forEach { account, transactions -> 
+        println(account.description)
+        transactions.ifEmpty { println("no payments") }
+        transactions.forEach { println("${it.description} ${it.amount.value} ${it.amount.currency}") }
+    }
+    
+    activeAccounts
+        .map { "Account ${it.description} has a balance of ${it.balance.value} ${it.balance.currency}" }
         .forEach { println(it) }
 }
+
+fun getPayments(id: Int) = Payment
+    .list(id, Pagination().urlParamsCountOnly)
+    .value
+
+private fun getActiveAccounts(): Set<MonetaryAccountBank> = MonetaryAccountBank
+    .list(Pagination().urlParamsCountOnly).value
+    .filter { it.status == "ACTIVE" }
+    .toSet()
+
 
 private fun loadAPIContext() {
     val key = File("key.txt").readText()
 
     val apiContext = ApiContext.create(
-        ApiEnvironmentType.SANDBOX,
+        ApiEnvironmentType.PRODUCTION,
         key,
         "Developer device"
     )
